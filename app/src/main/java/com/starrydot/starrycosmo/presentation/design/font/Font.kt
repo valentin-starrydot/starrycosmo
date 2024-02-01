ac5f8ea7b077ba1312e350fe30aefb11eff65ffb
Valentin <valentin@starrydot.fr>
2024-02-01 10:46:06 +0100

    Added common UI resources (font, color)

---
+++ app/src/main/java/com/starrydot/starrycosmo/presentation/design/font/Font.kt
@@ -0,0 +1,19 @@
+package com.starrydot.starrycosmo.presentation.design.font
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.starrydot.starrycosmo.R
+
+val FallingSky = FontFamily(
+    Font(R.font.fallingsky_light, FontWeight.Light),
+    Font(R.font.fallingsky_lightitalic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.fallingsky_normal, FontWeight.Normal),
+    Font(R.font.fallingsky_medium, FontWeight.Medium),
+    Font(R.font.fallingsky_mediumitalic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.fallingsky_semibold, FontWeight.SemiBold),
+    Font(R.font.fallingsky_semibolditalic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.fallingsky_black, FontWeight.Black),
+    Font(R.font.fallingsky_blackitalic, FontWeight.Black, FontStyle.Italic)
+)
